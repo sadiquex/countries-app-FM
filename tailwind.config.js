65725c87ae1b2fa518b91e3f0f9c759d48756c25
sadiquex <abubakasaddik1@gmail.com>
2024-05-23 11:28:46 +0000

    added theme toggle selector

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "selector",
   theme: {
     screens: {
-      sm: "320px",
+      sm: "480px",
       md: "768px",
       lg: "1024px",
       xl: "1280px",
